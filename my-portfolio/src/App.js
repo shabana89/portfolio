355104c0702a3b11f0736dceda14939e3255cad7
shabana kauser <shabanakauser89@gmail.com>
2022-08-16 23:46:25 +0100

    returned topbar component on app.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,11 @@
+import Topbar from "./components/topbar/Topbar.jsx";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Topbar />
+    </div>
+  );
 }
 
 export default App;
