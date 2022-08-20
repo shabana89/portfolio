e61f148a0ac1d2147735bb0c5237e7d445a97aac
shabana kauser <shabanakauser89@gmail.com>
2022-08-20 14:29:46 +0100

    Added material-icons for contact and mail. Added the item containers for contact and mail and added css styling to them

--- my-portfolio/src/components/topbar/Topbar.jsx
+++ my-portfolio/src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss";
+import { Person, Mail } from "@mui/icons-material";
 
 function Topbar() {
   return (
@@ -8,6 +9,14 @@ function Topbar() {
           <a href="#intro" className="logo">
             SK
           </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>07760903081</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>shabanakauser89@gmail.com</span>
+          </div>
         </div>
         <div className="right"> This is right</div>
       </div>
