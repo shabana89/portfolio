007820dffac0c8e7613a5d0e524b42ddc1ba8842
shabana kauser <shabanakauser89@gmail.com>
2022-08-20 21:13:30 +0100

    added useState for hamburger menu to activiate topbar

--- my-portfolio/src/components/topbar/Topbar.jsx
+++ my-portfolio/src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss";
 import { Person, Mail } from "@mui/icons-material";
 
-function Topbar() {
+function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -19,7 +19,7 @@ function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
