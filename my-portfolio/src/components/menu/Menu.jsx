f76b8ad3e0afe4be5eae4fbcbf6115c974de94b9
shabana kauser <shabanakauser89@gmail.com>
2022-08-22 21:08:27 +0100

    added onClick functions to the li components on menu and set these to false. So the menu side bar will close after each component has been selected to navigate straight to that page

--- my-portfolio/src/components/menu/Menu.jsx
+++ my-portfolio/src/components/menu/Menu.jsx
@@ -4,20 +4,20 @@ function Menu({ menuOpen, setMenuOpen }) {
   return (
     <div className={"menu " + (menuOpen && "active")}>
       <ul>
-        <li>
-          <a href="intro">Home</a>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#intro">Home</a>
         </li>
-        <li>
-          <a href="portfolio">Portfolio</a>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
-          <a href="works">Works</a>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#works">Works</a>
         </li>
-        <li>
-          <a href="testimonials">Testimonials</a>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#testimonials">Testimonials</a>
         </li>
-        <li>
-          <a href="contact">Contact</a>
+        <li onClick={() => setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
         </li>
       </ul>
     </div>
