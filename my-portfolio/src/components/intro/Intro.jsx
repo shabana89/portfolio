08e7fc475b095630591064eb932eb1b59b8a1f54
shabana kauser <shabanakauser89@gmail.com>
2022-08-22 23:51:24 +0100

    Created assets folder for images. Sectioned intro page into 2 parts with left and right div. Added my profil pic to intro page.

--- my-portfolio/src/components/intro/Intro.jsx
+++ my-portfolio/src/components/intro/Intro.jsx
@@ -1,7 +1,16 @@
 import "./intro.scss";
 
 function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.png" alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
 
 export default Intro;
