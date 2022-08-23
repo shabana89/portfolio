8d917de1ff72ddfe907373fe524194687a2803e9
shabana kauser <shabanakauser89@gmail.com>
2022-08-23 15:26:41 +0100

    addedityped extention for dynamic intro display. However some issue occuring and characters being doubled up. Will need to fix this

--- my-portfolio/src/components/intro/Intro.jsx
+++ my-portfolio/src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Designer", "Developer", "Content Creater"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +26,7 @@ function Intro() {
           <h2>Hi There, I'm</h2>
           <h1>Shabana Kauser</h1>
           <h3>
-            I am a..<span></span>
+            I am a... <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
