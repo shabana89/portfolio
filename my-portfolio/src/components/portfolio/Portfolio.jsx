ef1c628414c6002fee145592c65b06656011e0c2
shabana kauser <shabanakauser89@gmail.com>
2022-08-23 20:29:55 +0100

    Added styling to portfolio page and changed images to coming soon. Arranged the boxes for each p roject as i want. Will style more later

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -14,7 +14,42 @@ function Portfolio() {
       <div className="container">
         <div className="item">
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSFb0b4D0QjeeepuAa1nmcksjmNSC0dfRijA&usqp=CAU"
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://sussex.news/wp-content/uploads/2022/06/realistic-coming-soon-background_23-2148889082.jpg"
             alt=""
           />
           <h3>To-do List</h3>
