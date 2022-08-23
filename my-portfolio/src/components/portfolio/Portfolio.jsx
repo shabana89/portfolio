0878d87d663a3899f41c395e917dd22cd870fe69
shabana kauser <shabanakauser89@gmail.com>
2022-08-23 18:33:07 +0100

    Have started to arrange portfolio page to make the main template

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,27 @@
 import "./porfolio.scss";
 
 function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSFb0b4D0QjeeepuAa1nmcksjmNSC0dfRijA&usqp=CAU"
+            alt=""
+          />
+          <h3>To-do List</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
