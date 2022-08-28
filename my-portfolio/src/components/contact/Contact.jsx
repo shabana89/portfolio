6bfc9748a7f9b1dc4a5c04b2471254fcc477a94a
shabana kauser <shabanakauser89@gmail.com>
2022-08-28 23:08:00 +0100

    started adding left and right div  dividers to contact page and decided what will go in each section

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -1,7 +1,21 @@
 import "./contact.scss";
 
 function Contact() {
-  return <div className="contact" id="contact"></div>;
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/handshake.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
